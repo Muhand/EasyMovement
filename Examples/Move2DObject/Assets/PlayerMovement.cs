67bf6a54c8d46b1ff4dd6062ddf03dd992b7874c
muhand <muhandjumah@gmail.com>
2017-09-06 20:12:34 -0400

    Added Move2DObject class and translateOverLine function

---
+++ Examples/Move2DObject/Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyMovement;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //This can be any vector3, in this case we have a gameobject and then we will take it's position, simply instead of gameobject you can have mouse click, etc...
+    [SerializeField]
+    GameObject direction;       
+
+	void FixedUpdate () {
+        Move2DObject.translateOverLine(gameObject, direction.transform.position, 0.1f, 1000);
+	}
+}
